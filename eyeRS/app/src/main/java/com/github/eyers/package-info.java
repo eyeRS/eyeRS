1c7fea05efee202cff020193fe8a24d0312cc1f1
Blunderchips <matthewvdbijl@gmail.com>
2017-06-19 23:57:52 +0200

    nightly

--- eyeRS/app/src/main/java/com/github/eyers/package-info.java
+++ eyeRS/app/src/main/java/com/github/eyers/package-info.java
@@ -1,4 +1,10 @@
 /**
  * TODO.
+ *
+ * @author Matthew Van der Bijl
+ * @author Nathan Shava
+ * @author ​Emilde ​Arsenio
+ * @author ​Andrea ​Cloete
+ * @author Sajaad Ishmail
  */
 package com.github.eyers;
