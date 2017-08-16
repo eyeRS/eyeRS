ffb4f3b76867802c1ad32c63d5e9bab28e2c3c6e
Blunderchips <matthewvdbijl@gmail.com>
2017-08-16 08:59:48 +0200

    added help video template

--- eyeRS/app/src/main/java/com/github/eyers/activities/HelpActivity.java
+++ eyeRS/app/src/main/java/com/github/eyers/activities/HelpActivity.java
@@ -1,11 +1,17 @@
 package com.github.eyers.activities;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.VideoView;
+
 import com.github.eyers.R;
 
-public final class HelpActivity extends AppCompatActivity {
+public final class HelpActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +19,32 @@ public final class HelpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_help);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        findViewById(R.id.btnFeedback).setOnClickListener(this);
+
+        final VideoView videoView = (VideoView) findViewById(R.id.videoView);
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.test);
+        videoView.setVideoURI(uri);
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+                videoView.start();
+            }
+        });
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnFeedback:
+                Toast.makeText(this, "Take the user to the play store page to leavev a comment", Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 }
