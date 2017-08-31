7ab05e29517eddbfafa10364ec119f5f49116e7f
sajjaadishmail3 <sajjaadishmail@gmail.com>
2017-08-31 16:09:56 +0200

    Updated

--- eyeRS/app/src/main/java/com/github/eyers/activities/SearchActivity.java
+++ eyeRS/app/src/main/java/com/github/eyers/activities/SearchActivity.java
@@ -1,15 +1,23 @@
 package com.github.eyers.activities;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.github.eyers.R;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 public class SearchActivity extends AppCompatActivity {
+    /**
+     * Declare MaterialSearchView when used in activities
+     */
+    MaterialSearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,17 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        /** From here on copy the code to the necessary activities for functionality of the search
+         * fucntion.
+         */
+
+        getSupportActionBar().setTitle("Search Bar");
+        toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
+
+        MaterialSearchView searchView = (MaterialSearchView)findViewById(R.id.search_view);
+
+        /**
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +46,15 @@ public class SearchActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+         */
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_bar, menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+        return true;
+    }
 }
