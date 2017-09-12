85c2761aa3c5a6eda718e47723fdcbd9ef32bc94
Nathan Shava <nathantendai.shava@gmail.com>
2017-09-12 17:04:28 +0200

    Login layout update
    # Conflicts:
    #	eyeRS/app/src/main/java/com/github/eyers/MainActivity.java
    #	eyeRS/app/src/main/res/layout/activity_main.xml
    #	eyeRS/app/src/main/res/values/strings.xml

--- eyeRS/app/src/main/java/com/github/eyers/activities/LoginActivity.java
+++ eyeRS/app/src/main/java/com/github/eyers/activities/LoginActivity.java
@@ -3,6 +3,7 @@ package com.github.eyers.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -20,6 +21,8 @@ public final class LoginActivity extends AppCompatActivity implements View.OnCli
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         this.txtPIN = (EditText) findViewById(R.id.txtPIN);
 
