f2158cd821a452bc48c589950bb1d361d220d93b
sajjaadishmail3 <sajjaadishmail@gmail.com>
2017-10-12 15:00:36 +0200

    updated searchView

--- eyeRS/app/src/main/java/com/github/eyers/activities/DisplaySettingsActivity.java
+++ eyeRS/app/src/main/java/com/github/eyers/activities/DisplaySettingsActivity.java
@@ -64,7 +64,7 @@ public class DisplaySettingsActivity extends AppCompatActivity implements OnClic
             }
         });
     }
-
+    /**
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.imgbtn_Blue:
@@ -81,6 +81,7 @@ public class DisplaySettingsActivity extends AppCompatActivity implements OnClic
                 break;
         }
     }
+     */
 } //end class DisplaySettingsActivity
 
 
