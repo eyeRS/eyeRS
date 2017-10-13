8d2de834a163ac67cf94eb0908c1cf185f5dbbe6
sajjaadishmail3 <sajjaadishmail@gmail.com>
2017-10-13 12:08:22 +0200

    Assisting with share function. Let me know if there is a better way to implement the share.

--- eyeRS/app/src/main/java/com/github/eyers/activities/todo/ShareActivity.java
+++ eyeRS/app/src/main/java/com/github/eyers/activities/todo/ShareActivity.java
@@ -4,8 +4,13 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.content.Intent;
+import android.view.ActionProvider;
 
 import com.github.eyers.R;
 
@@ -27,6 +32,32 @@ public class ShareActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private ShareActionProvider mShareActionProvider;
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate menu resource file.
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.shareButton);
+
+        // Fetch and store ShareActionProvider
+//        mShareActionProvider = (ShareActionProvider) item.getActionProvider();
+
+
+        // Return true to display menu
+        return true;
+    }
+
+    // Call to update the share intent
+    private void setShareIntent(Intent shareIntent) {
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(shareIntent);
+        }
     }
 
 }
