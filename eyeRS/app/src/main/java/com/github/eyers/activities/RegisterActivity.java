3df9f196f2e51c5f95e77b7708056dc59a9b8f68
Nathan Shava <nathantendai.shava@gmail.com>
2017-10-25 23:40:28 +0200

    User validation
    # Conflicts:
    #	eyeRS/app/src/main/java/com/github/eyers/MainActivity.java
    #	eyeRS/app/src/main/res/layout/activity_main.xml
    #	eyeRS/app/src/main/res/values/strings.xml

--- eyeRS/app/src/main/java/com/github/eyers/activities/RegisterActivity.java
+++ eyeRS/app/src/main/java/com/github/eyers/activities/RegisterActivity.java
@@ -64,7 +64,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     private EditText txtPIN2;
     private EditText txtResponse;
     private Spinner spinner;
-    private Button registerButton;
+
     /**
      * Content resolver declaration
      */
@@ -96,8 +96,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         this.spinner.setAdapter(adapter);
         this.spinner.setOnItemSelectedListener(this);
 
-        this.registerButton = (Button) findViewById(R.id.btnRegister);
-        this.registerButton.setOnClickListener(this);
+        findViewById(R.id.btnRegister).setOnClickListener(this);
         findViewById(R.id.btnClearReg).setOnClickListener(this);
 
         if (savedInstanceState != null) {
@@ -154,10 +153,6 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
              * Navigate to the Login screen once registration has been successful
              */
             super.startActivity(new Intent(this, LoginActivity.class));
-            /**
-             * Disable the button after user has successfully registered
-             */
-            this.registerButton.setEnabled(false);
 
         } catch (Exception ex) {
 
